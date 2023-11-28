45b5dc8ed300ce545ac4c1198071d711274f4393
Ram Jondhale <ramjondhale1@gmail.com>
2023-11-28 23:24:07 +0530

    Enable event logging support

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
@@ -29,10 +30,20 @@ class ViewController: UIViewController {
             alertController.addAction(UIAlertAction(title: "It's cool", style: .default))
             self.present(alertController, animated: true)
         }
+
+        Analytics.trackEvent("navigate_to_calculator")
     }
 
     @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
-        Crashes.generateTestCrash()
+        // Crashes.generateTestCrash()
+
+        let current_age = Int(ageTextField.text ?? "0")
+        let planned_retirement_age = Int(retirementAgeTextField.text ?? "0")
+
+        let properties = ["current_age": String(current_age ?? 0),
+                          "planned_retirement_age": String(planned_retirement_age ?? 0)]
+
+        Analytics.trackEvent("calculate_retirement_amount", withProperties: properties)
     }
     
 }

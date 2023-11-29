59f979b8a0396108e4a423dfd61076e1b9378b3d
Ram Jondhale <ramjondhale1@gmail.com>
2023-11-29 23:58:42 +0530

    Retirement calculation

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -34,19 +34,33 @@ class ViewController: UIViewController {
         Analytics.trackEvent("navigate_to_calculator")
     }
 
+    func calculateRetirementAmount(current_age: Int, retirement_age: Int, monthly_investment: Float, current_savings: Float, interest_rate: Float) -> Double {
+        let months_until_retirement = (retirement_age - current_age) * 12
+
+        var retirement_amount = Double(current_savings) * pow(Double(1 + interest_rate / 100), Double(months_until_retirement))
+
+        let monthly_rate = Double(interest_rate / 100 / 12)
+        let total_investments = Double(monthly_investment) * ((pow(1 + monthly_rate, Double(months_until_retirement)) - 1) / monthly_rate)
+
+        retirement_amount += total_investments
+
+        return retirement_amount
+    }
+
     @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
-        // Crashes.generateTestCrash()
+        // MSCrashes.generateTestCrash()
+        let current_age : Int? = Int(ageTextField.text!)
+        let planned_retirement_age : Int? = Int(retirementAgeTextField.text!)
+        let monthly_investment : Float? = Float(monthlyInvestmentsTextField.text!)
+        let current_savings : Float? = Float(savingsTextField.text!)
+        let interest_rate : Float? = Float(interestRateTextField.text!)
 
-        let current_age = Int(ageTextField.text ?? "0")
-        let planned_retirement_age = Int(retirementAgeTextField.text ?? "0")
-        let monthly_investment = Float(monthlyInvestmentsTextField.text ?? "0")
-        let current_savings = Float(savingsTextField.text ?? "0")
-        let interest_rate = Float(interestRateTextField.text ?? "0")
+        let retirement_amount = calculateRetirementAmount(current_age: current_age!, retirement_age: planned_retirement_age!, monthly_investment: monthly_investment!, current_savings: current_savings!, interest_rate: interest_rate!)
 
-        resultLabel.text = "If you save $\(monthly_investment!) every month for \(planned_retirement_age! - current_age!) years, and invest that money plus your current investment of $\(current_savings!) at a \(interest_rate!)% annual interest rate, you will have $X by the time you are \(planned_retirement_age!)"
+        resultLabel.text = "If you save $\(monthly_investment!) every month for \(planned_retirement_age! - current_age!) years, and invest that money plus your current investment of $\(current_savings!) at a \(interest_rate!)% anual interest rate, you will have $\(retirement_amount) by the time you are \(planned_retirement_age!)"
 
-        let properties = ["current_age": String(current_age ?? 0),
-                          "planned_retirement_age": String(planned_retirement_age ?? 0)]
+        let properties = ["current_age": String(current_age!),
+                          "planned_retirement_age": String(planned_retirement_age!)]
 
         Analytics.trackEvent("calculate_retirement_amount", withProperties: properties)
     }

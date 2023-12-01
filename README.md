34fa5304855781ba74aa177574bcff5ba1e3b4cf
Ram Jondhale <31801085+ramjondhale@users.noreply.github.com>
2023-12-01 22:44:21 +0530

    Revert "Merge test into main"

--- README.md
+++ README.md
@@ -7,7 +7,4 @@ Test: [![Build status](https://build.appcenter.ms/v0.1/apps/3427e3c6-65fe-467f-a
 
 Main: [![Build status](https://build.appcenter.ms/v0.1/apps/3427e3c6-65fe-467f-a135-2f77c97c51b7/branches/main/badge)](https://appcenter.ms)
 
-Hello
-
-
 
--- RetirementCalculatorUITests/RetirementCalculatorUITests.swift
+++ RetirementCalculatorUITests/RetirementCalculatorUITests.swift
@@ -10,59 +10,32 @@ import XCTest
 final class RetirementCalculatorUITests: XCTestCase {
 
     override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() throws {
-        XCTContext.runActivity(named: "screenshot") { activity in
-            let app = XCUIApplication()
-            app.launch()
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
 
-            app.textFields["Monthly investments"].tap()
-            let key = app.keys["5"]
-            key.tap()
-            let key2 = app/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            key2.tap()
-            key2.tap()
-            key2.tap()
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
 
-            let yourCurrentAgeTextField = app.textFields["Your current age"]
-            yourCurrentAgeTextField.tap()
-            let key3 = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            key3.tap()
-            let key4 = app/*@START_MENU_TOKEN@*/.keys["3"]/*[[".keyboards.keys[\"3\"]",".keys[\"3\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            key4.tap()
-
-            let yourPlannedRetirementAgeTextField = app.textFields["Your planned retirement age"]
-            yourPlannedRetirementAgeTextField.tap()
-
-            let key5 = app/*@START_MENU_TOKEN@*/.keys["4"]/*[[".keyboards.keys[\"4\"]",".keys[\"4\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            key5.tap()
-            let key6 = app.keys["0"]
-            key6.tap()
-
-            let averageInterestRateOfInvestmentsTextField = app.textFields["Average interest rate of investments"]
-            averageInterestRateOfInvestmentsTextField.tap()
-
-            let key7 = app/*@START_MENU_TOKEN@*/.keys["8"]/*[[".keyboards.keys[\"8\"]",".keys[\"8\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            key7.tap()
-
-            app.textFields["Current savings"].tap()
-            key5.tap()
-            key2.tap()
-            key2.tap()
-            key2.tap()
-            key2.tap()
-            key2.tap()
-            app/*@START_MENU_TOKEN@*/.staticTexts["Calculate retirement amount"]/*[[".buttons[\"Calculate retirement amount\"].staticTexts[\"Calculate retirement amount\"]",".staticTexts[\"Calculate retirement amount\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-
-            let resultLabel = app.staticTexts.element(matching: .any, identifier: "ResultLabelA11yID")
-            let resultText = "If you save $5000.0 every month for 17 years, and invest that money plus your current investment of $400000.0 at a 8.0% annual interest rate, you will have $2633358500797.138 by the time you are 40"
-            XCTAssertEqual(resultLabel.label, resultText)
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
         }
     }
 }
